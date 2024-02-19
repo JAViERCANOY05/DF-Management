773e37fe13395ada2f9112c29f4f527500a4ec31
JavierCanoy <javier@mabolotech.com>
2024-02-20 07:48:01 +0800

    api-getCont

---
+++ src/pages/api/getcontribution.ts
@@ -0,0 +1,29 @@
+const GetContribution = {
+  get: async (token: any) => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/getContribution`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (response.ok) {
+        const responseData = await response.json();
+        const sendTo = {
+          response: responseData,
+          status: true,
+        };
+        return sendTo
+      }
+      throw new Error("Something went wrong!");
+    } catch (error) {
+      throw error;
+    }
+  },
+};
+
+export default GetContribution;

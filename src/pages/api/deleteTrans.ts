718a69c9fbb7c543e11810dbb1ef70a61bd142d0
JavierCanoy <javier@mabolotech.com>
2024-03-09 17:20:01 +0800

    api-deletetrans

---
+++ src/pages/api/deleteTrans.ts
@@ -0,0 +1,29 @@
+const DeleteTrans = {
+    delete: async (token: any, id: any) => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/deleteTransaction/${id}`,
+          {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        if (response.ok) {
+          const responseData = await response.json();
+          const sendTo = {
+            response: responseData,
+            status: true,
+          };
+          return sendTo;
+        }
+        throw new Error("Semething went wrong!");
+      } catch (error) {
+        throw error;
+      }
+    },
+  };
+  export default DeleteTrans;
+  
